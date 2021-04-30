e7897a66ea9e2438347bb7a60bc093a69887bbda
naretayami <naretayami29@gmail.com>
2021-04-30 16:18:18 +0900

    change

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,12 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-# import languages
+import languages
+
 
 bot = ChatBot(
         name='MyBot',
-        # tagger_language=languages.JPN  # 日本語設定
+        tagger_language=languages.JPN  # 日本語設定
+        # database_uri='mysql://root:password@127.0.0.1:3306/db_name' # mysql://root:パスワード@ホスト:ポート/データベース名
         # tagger_language=languages.GINZA
 )
 
