815e17e733e3537219e4f1b1893061c78ed96710
naretayami <naretayami29@gmail.com>
2021-04-30 14:39:51 +0900

    chatter bot start

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+# import languages
+
+bot = ChatBot(
+        name='MyBot',
+        # tagger_language=languages.JPN  # 日本語設定
+        # tagger_language=languages.GINZA
+)
+
+trainer = ChatterBotCorpusTrainer(bot)
+
+trainer.train(
+        'chatterbot.corpus.japanese' # 日本語版コーパスを設定
+)
+
+while True:
+    try:
+        input_data = input('YOU: ')
+        response = bot.get_response(input_data)
+        print('{}: {}'.format(bot.name, response))
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
