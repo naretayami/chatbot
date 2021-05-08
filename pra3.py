f93e425af92a08f6d0cb4319b68da42a14004de3
naretayami <naretayami29@gmail.com>
2021-05-08 20:40:20 +0900

    あまり進んでない

---
+++ pra3.py
@@ -0,0 +1,12 @@
+import spacy
+nlp = spacy.load("ja_core_news_sm")
+
+
+text = "静岡県にある三保の松原は世界遺産の一部です。"
+doc = nlp(text)
+for token in doc:
+    print(token.text)
+for ent in doc.ents:
+    print(ent.text, ent.label_)
+mihonomatsubara = doc[4:7]
+print("Missing entity:", mihonomatsubara.text)
