dbcf55d3f50aed4b6de82ca7e79314b072880eee
naretayami <naretayami29@gmail.com>
2021-05-15 00:01:56 +0900

    うまくいかない

--- chatbot1/chatbot.py
+++ chatbot1/chatbot.py
@@ -1,30 +1,30 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer,ChatterBotCorpusTrainer, ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
 import languages
 
 
 bot = ChatBot(
         name='MyBot',
         tagger_language=languages.JPN,  # 日本語設定
-        storage_adapter='chatterbot.storage.SQLStorageAdapter',
+        # storage_adapter='chatterbot.storage.SQLStorageAdapter',
         # logic_adapters=[
         #     'chatterbot.logic.MathematicalEvaluation'
         # ],
-        database_uri='sqlite:///mydb.chatbot',
+        database_uri='mysql://root:renarena@localhost:3306/chatbot?charset=utf8mb4',
 
 )
 
-conversarion = [
-    '勉強したくない',
-    'もう少し頑張れ！',
-    '疲れた',
-    '5分休憩したらどう？',
-    'お腹すいた',
-    'チョコがお勧めだよ',
-]
+# conversarion = [
+#     '勉強したくない',
+#     'もう少し頑張れ！',
+#     '疲れた',
+#     '5分休憩したらどう？',
+#     'お腹すいた',
+#     'チョコがお勧めだよ',
+# ]
 
-trainer = ListTrainer(bot)
-trainer.train(conversarion)
+# trainer = ListTrainer(bot)
+# trainer.train(conversarion)
 
 trainer = ChatterBotCorpusTrainer(bot)
 trainer.train(
