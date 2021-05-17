07bda9c7d7aa1a47cf151b51b0acd8d8d10f6fb6
naretayami <naretayami29@gmail.com>
2021-05-17 15:06:14 +0900

    training

--- chatbot4/training.py
+++ chatbot4/training.py
@@ -6,10 +6,9 @@ bot = ChatBot(
         name='MyBot',
         read_only=True,
         tagger_language=languages.JPN ,
-        database_uri='mysql://root:renarena@localhost:3306/chatbot?charset=utf8mb4'
 )
 
-conversarion = [
+conversation = [
     '勉強したくない',
     'もう少し頑張れ！',
     '疲れた',
@@ -18,10 +17,24 @@ conversarion = [
     'チョコがお勧めだよ',
     'なんにもしたくない',
     '一度寝よう！',
+    'くま',
+    '<img id="kuma" src="/static/images/kuma.png" alt="くま"></img>
+]
+
+conversation2 = [
+        '数学が分からない',
+        '何が分からないの？',
+        '連立方程式',
+        'おすすめサイト　https://math.005net.com/yoten/renrituKagen.php',
+        '三角比',
+        '難しいよね　https://www.youtube.com/watch?v=OLqgs4fJl7Y',
+        '二次関数',
+        '先生にも聞いてみよう　https://www.studyplus.jp/351'
 ]
 
 trainer = ListTrainer(bot)
-trainer.train(conversarion)
+trainer.train(conversation)
+trainer.train(conversation2)
 
 trainer = ChatterBotCorpusTrainer(bot)
 
