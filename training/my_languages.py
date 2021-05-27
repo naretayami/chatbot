354510f75f4acd22c608c360047d5bd9085c074e
naretayami <78332978+naretayami@users.noreply.github.com>
2021-05-27 10:00:52 +0900

    Update my_languages.py

--- training/my_languages.py
+++ training/my_languages.py
@@ -5,6 +5,6 @@ class GINZA:
 
 
 # class JPN:
-#     ISO_639_1 = 'ja_core_news_sm' # jaからja_core_news_smに変更
+#     ISO_639_1 = 'ja_core_news_sm'
 #     ISO_639 = 'jpn'
 #     ENGLISH_NAME = 'Japanese'
